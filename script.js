10714554a8507d29d0a842b567b097f7ef57ed0b
sekkond2004 <nguyen.ekko@ic.edu>
2025-10-19 17:54:04 -0500

    answer up to problem 9

--- script.js
+++ script.js
@@ -6,28 +6,31 @@ let favoriteFoods = ["Chicken", "Durian", "Broccoli", "Beef", "Squid",
 // 2. Loop through the list and print: "One of my favorite foods is ______."
 for (let i = 0; i < favoriteFoods.length; i++) {
   console.log("One of my favorite foods is " + favoriteFoods[i]);
-}
+};
 
 // 3. Print out the rating for each food with a ranking like:
 // "My #1 favorite food is Ramen" (copy/paste for all items)
 // "My #2 favorite food is Sushi"
 // ...etc.
-
-
-
-
+let ranking = 1;
+for (let i = 0; i < favoriteFoods.length; i++) {
+  console.log("My #" + ranking++ + " favorite foods is " + favoriteFoods[i]);
+};
 
 // 4a. Create a function printFoodRecommendation(foodName) that prints out the following for the foodName provided
     // "Have you ever tried ____?"
     // "I always recommend ____ to friends."
     // "Trust me — ____ is delicious."
-
-
-
+function printFoodRecommendation(foodName) {
+  console.log("Have you ever tried " + foodName);
+  console.log("I always recommend "+ foodName + " to friends.");
+  console.log("Trust me, "+ foodName + " is delicious")
+}
 
 // 4b. Call the function at least 3 times
-
-
+printFoodRecommendation(favoriteFoods[3])
+printFoodRecommendation(favoriteFoods[0])
+printFoodRecommendation(favoriteFoods[4])
 
 // Here's a list of 50 friends' favorite foods:
 let friendFavorites = [
@@ -35,24 +38,44 @@ let friendFavorites = [
   ];
 
 // 5. Print out only foods that have an "a" in the name. For example, "Pizza" would not be included, but "Pasta" would be.
-
-
+for (i = 0; i < friendFavorites.length; i++) {
+  if (friendFavorites[i].includes("a"))
+  console.log(friendFavorites[i])
+}
 
 // 6. Store the result in an array called foodsWithA. Print out the array.
-
-
+let foodsWithA = [];
+for (i = 0; i < friendFavorites.length; i++) {
+  if (friendFavorites[i].includes("a"))
+    foodsWithA.push(friendFavorites[i]);
+  }
+    console.log(foodsWithA);
 
 // 7. Create a new array longFoodNames for foods with names longer than 6 characters.
-
-
-
+let longFoodNames = []
+for (i = 0; i < friendFavorites.length; i++) {
+  if (friendFavorites[i].length > 6) {
+    longFoodNames.push(friendFavorites[i]);
+ }
+}
 // 8. Create another array shortFoodNames for foods 6 characters or shorter.
-
-
+let shortFoodNames = []
+for (i = 0; i < friendFavorites.length; i++) {
+  if (friendFavorites[i].length <= 6) {
+    shortFoodNames.push(friendFavorites[i]);
+ }
+}
 
 // 9. Print both arrays and compare:
 // "There are more long-named foods." OR "There are more short-named foods."
+console.log(longFoodNames);
+console.log(shortFoodNames);
 
-
+if (longFoodNames.length > shortFoodNames.length) {
+  console.log("There are more long-named foods.")
+}
+else {
+  console.log("There are more short-named foods.")
+}
 // 10. STRETCH: Find the longest food name and print:
 // "The longest food name in the list is ______ with ___ characters."
